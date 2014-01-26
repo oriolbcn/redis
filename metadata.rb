c520434fadc1f9aa16b11bce08cf0721dbc013bf
Oriol Collell <oriol.collell.bcn@gmail.com>
2014-01-26 18:54:53 +0100

    decreased max memory

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "redis"
 maintainer        "Oriol Collell"
 maintainer_email  "ocollell@dineyo.com"
 description       "Installs redis from rwky's ppa"
-version           "0.0.6"
+version           "0.0.7"
 
 recipe "redis", "Installs redis"
 
